cb7f3a07a0ebc27d6dc3356cefc2e56d1c8d1cfe
obviouslyada <ada.ml.sim@gmail.com>
2016-10-23 11:47:58 -0400

    Add files via upload

--- export.php
+++ export.php
@@ -15,13 +15,20 @@ if ($conn->connect_error) {
 echo "Connected successfully!";
 echo "<br>";
 
-$sql = "SELECT * FROM Buyers
-INTO OUTFILE 'C:\Documents and Settings\All Users\Application Data\Buyers.csv'
-FIELDS ESCAPED BY ','
-TERMINATED BY ','
-ENCLOSED BY '\"'
-LINES TERMINATED BY '\r\n';
-";
+$sql = "SELECT 'id', 'FirstName', 'LastName', 'Email', 'Address1', 'Address2'
+		'City', 'Province', 'Postal_Code', 'Country', 'Phone', 'Email_Contact', 'Post_Contact',
+		'Sellers_ID', 'Donation', 'Means_of_Payment', 'reg_date'
+	   UNION ALL
+       SELECT id, FirstName, LastName, Email, Address1, Address2
+		City, Province, Postal_Code, Country, Phone, Email_Contact, Post_Contact,
+		Sellers_ID, Donation, Means_of_Payment, reg_date
+       FROM BUYERS
+	   INTO OUTFILE '/tmp/Buyers.csv'
+	   FIELDS ESCAPED BY ','
+	   TERMINATED BY ','
+	   ENCLOSED BY '\"'
+	   LINES TERMINATED BY '\r\n'";
+
 $result = $conn->query($sql);
 echo "Outputted Buyers <br>";
 
